236edb18c9fdaeef7591f02ee403239be802ca99
Christopher <chris-banyard@live.com>
2013-06-20 01:43:49 -0400

    Submit 1

---
+++ x100/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace x100
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openBtn = new System.Windows.Forms.Button();
+            this.listBtn = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 77);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(559, 375);
+            this.textBox1.TabIndex = 0;
+            // 
+            // openBtn
+            // 
+            this.openBtn.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.openBtn.Font = new System.Drawing.Font("MS Reference Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.openBtn.Location = new System.Drawing.Point(6, 462);
+            this.openBtn.Name = "openBtn";
+            this.openBtn.Size = new System.Drawing.Size(148, 60);
+            this.openBtn.TabIndex = 1;
+            this.openBtn.Text = "Open";
+            this.openBtn.UseVisualStyleBackColor = false;
+            this.openBtn.Click += new System.EventHandler(this.openBtn_Click);
+            // 
+            // listBtn
+            // 
+            this.listBtn.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.listBtn.Font = new System.Drawing.Font("MS Reference Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBtn.Location = new System.Drawing.Point(160, 462);
+            this.listBtn.Name = "listBtn";
+            this.listBtn.Size = new System.Drawing.Size(148, 60);
+            this.listBtn.TabIndex = 2;
+            this.listBtn.Text = "List";
+            this.listBtn.UseVisualStyleBackColor = false;
+            this.listBtn.Click += new System.EventHandler(this.listBtn_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(6, 528);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(559, 29);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("MS Reference Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.checkBox1.Location = new System.Drawing.Point(317, 462);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(54, 20);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = ".exe";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Font = new System.Drawing.Font("MS Reference Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.checkBox2.Location = new System.Drawing.Point(317, 483);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(51, 20);
+            this.checkBox2.TabIndex = 5;
+            this.checkBox2.Text = ".jpg";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Font = new System.Drawing.Font("MS Reference Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.checkBox3.Location = new System.Drawing.Point(317, 505);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(55, 20);
+            this.checkBox3.TabIndex = 6;
+            this.checkBox3.Text = ".png";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // vScrollBar1
+            // 
+            this.vScrollBar1.Location = new System.Drawing.Point(540, 90);
+            this.vScrollBar1.Name = "vScrollBar1";
+            this.vScrollBar1.Size = new System.Drawing.Size(22, 352);
+            this.vScrollBar1.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS Reference Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(10, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 60);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "x100";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SteelBlue;
+            this.ClientSize = new System.Drawing.Size(568, 560);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.listBtn);
+            this.Controls.Add(this.openBtn);
+            this.Controls.Add(this.vScrollBar1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "x100";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button openBtn;
+        private System.Windows.Forms.Button listBtn;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.VScrollBar vScrollBar1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ x100/Form1.cs
@@ -0,0 +1,208 @@
+﻿/***************************************************************************
+ * x100 Open Source Project         June 2013                              *
+ * Original code by Christopher Banyard                                    *
+ * *Code may be replicated and manipulated under the Open Source lisencing *
+ *  while retaining original name.                                         *
+ **************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace x100
+
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openBtn_Click(object sender, EventArgs e)
+        {
+              try
+            {
+                if (checkBox1.Checked == true)  // Open .exe
+                {
+                    string[] filesList = Directory.GetFiles(@"C:\Program Files (x86)\", "*.exe", SearchOption.AllDirectories);
+
+                    string[] filesList2 = Directory.GetFiles(@"C:\Program Files\", "*.exe", SearchOption.AllDirectories);
+
+                    progressBar1.Maximum = filesList.Length + filesList2.Length;
+                    progressBar1.Minimum = 0;
+                    progressBar1.Step = 1;
+
+                    #region
+                    foreach (string location in filesList)
+                    {
+                        System.Diagnostics.Process.Start(@location);
+                        System.Console.WriteLine(location);
+                        textBox1.AppendText(location);
+                        progressBar1.PerformStep();
+                    }
+                   
+                    foreach (string location in filesList2)
+                    {
+                        System.Diagnostics.Process.Start(@location);
+                        System.Console.WriteLine(location);
+                        textBox1.AppendText(location);
+                        progressBar1.PerformStep();
+                    }
+                    #endregion
+                    MessageBox.Show("Successfully opened all files!");
+                }
+            
+
+                else if (checkBox2.Checked == true)  // Open .jpg
+                {
+                    string[] filesList = Directory.GetFiles(@"C:\Users\", "*.jpg",
+                                                 SearchOption.AllDirectories);
+
+                    progressBar1.Maximum = filesList.Length;
+                    progressBar1.Minimum = 0;
+                    progressBar1.Step = 1;
+
+                    #region
+                    foreach (string location in filesList)
+                    {
+                        System.Diagnostics.Process.Start(@location);
+                        System.Console.WriteLine(location);
+                        textBox1.AppendText(location);
+                        progressBar1.PerformStep();
+                    }
+                    MessageBox.Show("Successfully opened all files!");
+                    #endregion
+                }
+
+                  else if (checkBox3.Checked == true)  // Open .png
+                  {
+                      string[] filesList = Directory.GetFiles(@"C:\Users\", "*.png",
+                                                   SearchOption.AllDirectories);
+
+                      progressBar1.Maximum = filesList.Length;
+                      progressBar1.Minimum = 0;
+                      progressBar1.Step = 1;
+
+                      #region
+                      foreach (string location in filesList)
+                      {
+                          System.Diagnostics.Process.Start(@location);
+                          System.Console.WriteLine(location);
+                          textBox1.AppendText(location);
+                          progressBar1.PerformStep();
+                      }
+                      MessageBox.Show("Successfully opened all files!");
+                      #endregion
+                  }
+                  else 
+                      MessageBox.Show("Be sure to check off what file type you would like to open/list"); 
+              }
+            
+            catch (Exception)
+            {
+                System.Console.WriteLine("Exception caught");
+                MessageBox.Show("Failed!");
+            }
+            System.Console.WriteLine("Done");  
+        }
+
+        private void listBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (checkBox1.Checked == true)  // List .exe
+                {
+                    string[] filesList = Directory.GetFiles(@"C:\Program Files (x86)\", "*.exe", SearchOption.AllDirectories);
+
+                    string[] filesList2 = Directory.GetFiles(@"C:\Program Files\", "*.exe", SearchOption.AllDirectories);
+
+                    progressBar1.Maximum = filesList.Length + filesList2.Length;
+                    progressBar1.Minimum = 0;
+                    progressBar1.Step = 1;
+
+                    #region
+                    foreach (string location in filesList)
+                    {
+                        System.Console.WriteLine(location);
+                        textBox1.AppendText(location);
+                        progressBar1.PerformStep();
+                    }
+                 
+                    foreach (string location2 in filesList2)
+                    {
+                        System.Console.WriteLine(location2);
+                        textBox1.AppendText(location2);
+                        progressBar1.PerformStep();
+                    }
+                    #endregion
+                   
+                    MessageBox.Show("Successfully listed all files!");
+                }
+
+
+                else if (checkBox2.Checked == true)   // List .jpg
+                {
+                    string[] filesList = Directory.GetFiles(@"C:\Users\", "*.jpg",
+                                                 SearchOption.AllDirectories);
+
+                    progressBar1.Maximum = filesList.Length;
+                    progressBar1.Minimum = 0;
+                    progressBar1.Step = 1;
+
+                    #region
+                    foreach (string location in filesList)
+                    {
+                        System.Console.WriteLine(location);
+                        textBox1.AppendText(location);
+                        progressBar1.PerformStep();
+                    }
+                    MessageBox.Show("Successfully opened all files!");
+                    #endregion
+                }
+
+                else if (checkBox3.Checked == true)  // List .png
+                {
+                    string[] filesList = Directory.GetFiles(@"C:\Users\", "*.png",
+                                                 SearchOption.AllDirectories);
+
+                    progressBar1.Maximum = filesList.Length;
+                    progressBar1.Minimum = 0;
+                    progressBar1.Step = 1;
+
+                    #region
+                    foreach (string location in filesList)
+                    {
+                        System.Console.WriteLine(location);
+                        textBox1.AppendText(location);
+                        progressBar1.PerformStep();
+                    }
+                    MessageBox.Show("Successfully opened all files!");
+                    #endregion
+
+                }
+                else 
+                    MessageBox.Show("Be sure to check off what file type you would like to open/list!");
+            }
+
+            catch (Exception)
+            {
+                System.Console.WriteLine("Exception caught");
+                MessageBox.Show("Failed!");
+            }
+            System.Console.WriteLine("Done");   
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
